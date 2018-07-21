9418f91ba531da91ebf38fbcdcbdd36be924748d
Necromancer <necromancerr@users.noreply.github.com>
2018-07-21 09:59:41 -0400

    first commit

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+trap 'echo "exiting ..."; exit' TERM INT
+
+ip=`ifconfig | awk '/ *inet addr:/ { match($0, "[0-9\.]+"); ip=substr($0, RSTART, RLENGTH); if (ip != "127.0.0.1") { print ip } }'`
+
+while true
+do
+	echo "waiting ..."
+	printf "`date '+%H:%M:%S'`    %-16s    %s" $ip $HOSTNAME | nc -q 0 -l 3333 > /dev/null &
+	wait $!
+	echo "new request served."
+done
